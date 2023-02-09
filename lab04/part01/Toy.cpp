74856c5b8eea9da184be7748bb735654fb02906a
Tushardeep Singh <tushardeepsingh97799@icloud.com>
2023-02-09 13:50:41 -0500

    added

--- lab04/part01/Toy.cpp
+++ lab04/part01/Toy.cpp
@@ -1,3 +1,11 @@
+// > I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
+/*
+Name : Tushardeep Singh
+seneca e-mail: tsingh245@myseneca.ca
+Seneca Student ID: 160427217
+completion date: 2nd Febuary'2023
+*/
 #include "Toy.h"
 
 sdds::Toy::Toy() = default;
